5000ec3b037c488e51a6254f7bc96c3e377f8f3c
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-23 23:52:18 +0600

    errorpage setup

--- src/Pages/Shared/ErrorPage/ErrorPage.js
+++ src/Pages/Shared/ErrorPage/ErrorPage.js
@@ -4,7 +4,28 @@ import PrimaryButton from '../../../Components/PrimaryButton/PrimaryButton';
 
 const ErrorPage = () => {
     return (
-        <div></div>
+        <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+            <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+                <div className='max-w-md text-center'>
+                <h2 className='mb-8 font-extrabold text-9xl text-red-500'>
+                    <span className='sr-only'>Error</span>
+                    <div className='flex justify-center items-center h-full'>
+                    4
+                    <div className='w-24 h-24 border-8 border-dotted rounded-full animate-spin mt-3 border-red-500'></div>
+                    4
+                    </div>
+                </h2>
+                <p className='text-2xl font-semibold md:text-3xl mb-8'>
+                    Sorry, we couldn't find this page.
+                </p>
+                <Link to='/'>
+                    <PrimaryButton classes='px-8 py-3 text-2xl font-semibold rounded'>
+                    Back to Homepage
+                    </PrimaryButton>
+                </Link>
+                </div>
+            </div>
+        </section>
     );
 };
 
