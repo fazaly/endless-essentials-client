d47cbe67e8c085e2c577951572ecb32d0fdec0f1
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-23 23:50:03 +0600

    auth setup

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,93 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+// Google Provider for sign in with google
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    // loading is set for better user interface
+    const [loading, setLoading] = useState(true);
+
+    // create user
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    // Sign In
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // Email Verify
+    const verifyEmail = () => {
+        setLoading(true)
+        return sendEmailVerification(auth.currentUser)
+    }
+
+    // State changed with onAuthState
+    useEffect(() => {
+        const unsubscribe =  onAuthStateChanged(auth,  (currentUser) => {
+            // console.log('user observing');
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => unsubscribe();
+    }, []);
+
+    // update user name & photo url
+    const updateUserProfile = (name, photo) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photo,
+        })
+    }
+
+    // Forget Password
+    const resetPassword = email => {
+        setLoading(true)
+        return sendPasswordResetEmail(auth, email)
+    }
+
+    // Sign Out
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // Google SignIn
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const authInfo = {
+        createUser,
+        signIn,
+        verifyEmail,
+        updateUserProfile,
+        user,
+        logOut,
+        loading,
+        setLoading,
+        resetPassword,
+        signInWithGoogle,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
