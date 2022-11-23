564998a985cdb36824caaf73d3791d0eb365b3f3
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-23 23:51:00 +0600

    router setup

---
+++ src/Routes/Router/Router.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import ErrorPage from "../../Pages/Shared/ErrorPage/ErrorPage";
+import SignUp from "../../Pages/SignUp/SignUp";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/home',
+                element: <Home/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <SignUp/>
+            }
+        ]
+    },
+]);
+
+export default router;
