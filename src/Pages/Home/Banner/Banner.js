1b383c11702ee42d601561fc4948618de3308dba
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-11-24 15:32:15 +0600

    footer added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <section className="dark:bg-gray-800 dark:text-gray-100 ml-[70px] mr-[70px]">
             <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-3 mx-auto sm:py-12  lg:flex-row lg:justify-between">
                 <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                    <h1 className="text-5xl font-bold leading-none text-gray-600 sm:text-6xl">Stay Connected Everywhere You Go!
+                    <h1 className="lg:text-5xl font-bold leading-none text-gray-600 sm:text-3xl">Stay Connected Everywhere You Go!
                     </h1>
                     <p className="mt-6 mb-8 text-lg sm:mb-12">Buy and sell means an agreement between the buyer and the seller whereby the seller has the duty to transfer the ownership of property to the buyer and the buyer pays the price of the property to the seller.
                     </p>
